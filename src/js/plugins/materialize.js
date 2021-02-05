4b1da7e2ab3022d9a468c1abb25c48d7316daa7b
Max Medvediev <medvediev.m91@gmail.com>
2021-02-05 23:17:34 +0200

    connect materialize and add nav in header

---
+++ src/js/plugins/materialize.js
@@ -0,0 +1,12 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+
+/* Init select */
+const select = document.querySelectorAll('select');
+M.FormSelect.init(select);
+
+
+export function getSelectInstanse(elem) {
+    return M.FormSelect.getInstance(elem);
+}
